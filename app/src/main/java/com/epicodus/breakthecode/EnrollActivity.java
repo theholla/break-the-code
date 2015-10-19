a9e0a109798402290ddcffb95474d2a237d83306
theholla <holla@bikefarm.org>
2015-10-19 14:28:53 -0700

    Add debug tool that writes to console

--- app/src/main/java/com/epicodus/breakthecode/EnrollActivity.java
+++ app/src/main/java/com/epicodus/breakthecode/EnrollActivity.java
@@ -5,15 +5,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.util.Log;
 
 public class EnrollActivity extends AppCompatActivity {
+
+    // getSimpleName() instead of hard coding name lets you refactor rename!
+    public static final String TAG = EnrollActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enroll);
 
-        //debugging tool to see if something works:
+        //debugging tool that speaks to the user:
 
         Toast.makeText(this, "Enroll activity works!!", Toast.LENGTH_LONG).show();
 
@@ -24,6 +28,11 @@ public class EnrollActivity extends AppCompatActivity {
         welcomeToast.show();
 
         */
+
+        //debugging tool to write to console:
+        // to find in console, filter by log level (returns at and below level) or search
+
+        Log.d(TAG, "We're logging from the Enroll Activity OnCreate() method");
 
     }
 }
